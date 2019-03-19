2185d41ce9fa4b087e61abe666ff0c75f35e15cc
Alexandru <alexandrusolot@gmail.com>
2019-03-19 12:29:03 +0000

    refactored the launch files and the launch bash scripts

---
+++ bash/network_config.sh
@@ -0,0 +1,4 @@
+host_address=$(hostname -I)
+
+ROS_MASTER_URI="http://$host_address:11311"
+ROS_HOSTNAME=host_address
---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# This script is intended to auto-start all nodes and launch files
+
+if [ "$1" = "real" ]; then
+    echo "here it goes the real stuff"
+else
+    echo "launching deployment sequence for simulation"
+
+    echo "launching robots in gazebo"
+    gnome-terminal -x sh -c "echo 'launching robots in gazebo'; roslaunch turtlebot3_formation main.launch"
+    sleep 0.5
+
+    echo "launching slam"
+    gnome-terminal -x sh -c "echo 'launching slam'; bash"
+    sleep 0.5
+
+    echo "launching leader navigation"
+    gnome-terminal -x sh -c "echo 'leader navigation'; bash"
+    sleep 0.5
+
+    echo "launching follower1 navigation"
+    gnome-terminal -x sh -c "echo 'follower1 navigation'; bash"
+    sleep 0.5
+
+    echo "launching follower2 navigation"
+    gnome-terminal -x sh -c "echo 'follower2 navigation'; bash"
+    sleep 0.5
+fi 
