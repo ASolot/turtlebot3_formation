f515c984dfcee9411d0f5bbdc51b4cc8ca19ebe9
Alexandru <alexandrusolot@gmail.com>
2019-03-19 21:37:07 +0000

    fixed multi-robot launch, and multirobot slam

--- deploy.sh
+++ deploy.sh
@@ -8,22 +8,22 @@ else
     echo "launching deployment sequence for simulation"
 
     echo "launching robots in gazebo"
-    gnome-terminal -x sh -c "echo 'launching robots in gazebo'; roslaunch turtlebot3_formation main.launch"
-    sleep 0.5
+    gnome-terminal -x sh -c "echo 'launching robots in gazebo'; roslaunch turtlebot3_formation main.launch; bash"
+    sleep 5
 
     echo "launching slam"
-    gnome-terminal -x sh -c "echo 'launching slam'; bash"
-    sleep 0.5
+    gnome-terminal -x sh -c "echo 'launching slam'; roslaunch turtlebot3_formation multirobot_slam.launch"
+    sleep 5
 
-    echo "launching leader navigation"
-    gnome-terminal -x sh -c "echo 'leader navigation'; bash"
-    sleep 0.5
+    # echo "launching leader navigation"
+    # gnome-terminal -x sh -c "echo 'leader navigation'; bash"
+    # sleep 0.5
 
-    echo "launching follower1 navigation"
-    gnome-terminal -x sh -c "echo 'follower1 navigation'; bash"
-    sleep 0.5
+    # echo "launching follower1 navigation"
+    # gnome-terminal -x sh -c "echo 'follower1 navigation'; bash"
+    # sleep 0.5
 
-    echo "launching follower2 navigation"
-    gnome-terminal -x sh -c "echo 'follower2 navigation'; bash"
-    sleep 0.5
+    # echo "launching follower2 navigation"
+    # gnome-terminal -x sh -c "echo 'follower2 navigation'; bash"
+    # sleep 0.5
 fi 
