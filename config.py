15770fbdf4789b6d8113263cad42a50697671800
Alexandru <alexandrusolot@gmail.com>
2019-03-22 22:14:55 +0000

    added config file. Moved all constants inside the config

---
+++ config.py
@@ -0,0 +1,16 @@
+# Better keep constants in a single safe place
+
+# Constants used for indexing.
+X = 0
+Y = 1
+YAW = 2
+
+# Constants for occupancy grid.
+FREE = 0
+UNKNOWN = 1
+OCCUPIED = 2
+
+ROBOT_RADIUS = 0.105 / 2.
+
+SPEED = .2
+EPSILON = .1
