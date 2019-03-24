cb5b53310680fa1e39bf2d554ea89e7f5de46933
Alexandru <alexandrusolot@gmail.com>
2019-03-24 11:55:33 +0000

    path planners defined

---
+++ python/path_planners.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+
+
+class SLAM(object):
+  def __init__(self):
+    rospy.Subscriber('/map', OccupancyGrid, self.callback)
+    self._tf = TransformListener()
+    self._occupancy_grid = None
+    self._pose = np.array([np.nan, np.nan, np.nan], dtype=np.float32)
+    
+  def callback(self, msg):
+    values = np.array(msg.data, dtype=np.int8).reshape((msg.info.width, msg.info.height))
+    processed = np.empty_like(values)
+    processed[:] = rrt.FREE
+    processed[values < 0] = rrt.UNKNOWN
+    processed[values > 50] = rrt.OCCUPIED
+    processed = processed.T
+    origin = [msg.info.origin.position.x, msg.info.origin.position.y, 0.]
+    resolution = msg.info.resolution
+    self._occupancy_grid = rrt.OccupancyGrid(processed, origin, resolution)
+
+  def update(self):
+    # Get pose w.r.t. map.
+    a = 'occupancy_grid'
+    b = 'base_link'
+    if self._tf.frameExists(a) and self._tf.frameExists(b):
+      try:
+        t = rospy.Time(0)
+        position, orientation = self._tf.lookupTransform('/' + a, '/' + b, t)
+        self._pose[X] = position[X]
+        self._pose[Y] = position[Y]
+        _, _, self._pose[YAW] = euler_from_quaternion(orientation)
+      except Exception as e:
+        print(e)
+    else:
+      print('Unable to find:', self._tf.frameExists(a), self._tf.frameExists(b))
+    pass
+
+  @property
+  def ready(self):
+    return self._occupancy_grid is not None and not np.isnan(self._pose[0])
+
+  @property
+  def pose(self):
+    return self._pose
+
+  @property
+  def occupancy_grid(self):
+    return self._occupancy_grid
+
+
+
+
+
+def get_path(final_node):
+  # Construct path from RRT solution.
+  if final_node is None:
+    return []
+  path_reversed = []
+  path_reversed.append(final_node)
+  while path_reversed[-1].parent is not None:
+    path_reversed.append(path_reversed[-1].parent)
+  path = list(reversed(path_reversed))
+  # Put a point every 5 cm.
+  distance = 0.05
+  offset = 0.
+  points_x = []
+  points_y = []
+  for u, v in zip(path, path[1:]):
+    center, radius = rrt.find_circle(u, v)
+    du = u.position - center
+    theta1 = np.arctan2(du[1], du[0])
+    dv = v.position - center
+    theta2 = np.arctan2(dv[1], dv[0])
+    # Check if the arc goes clockwise.
+    clockwise = np.cross(u.direction, du).item() > 0.
+    # Generate a point every 5cm apart.
+    da = distance / radius
+    offset_a = offset / radius
+    if clockwise:
+      da = -da
+      offset_a = -offset_a
+      if theta2 > theta1:
+        theta2 -= 2. * np.pi
+    else:
+      if theta2 < theta1:
+        theta2 += 2. * np.pi
+    angles = np.arange(theta1 + offset_a, theta2, da)
+    offset = distance - (theta2 - angles[-1]) * radius
+    points_x.extend(center[X] + np.cos(angles) * radius)
+    points_y.extend(center[Y] + np.sin(angles) * radius)
+  return zip(points_x, points_y)
